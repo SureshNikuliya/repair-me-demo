9071874faa2917745186fbf7f9aff26bcff09365
Suresh Nikuliya1 <suresh.nikuliya1@ibm.com>
2022-08-14 01:27:52 +0530

    Update Name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Repair Me Demo
+        Repair Me Now
       </header>
     </div>
   );
